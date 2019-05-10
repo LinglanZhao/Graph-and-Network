2bb3c668b07df3560548d4ca30517b57d1b8a75a
LinglanZhao <38338601+LinglanZhao@users.noreply.github.com>
2019-05-10 11:37:59 +0800

    Add files via upload

---
+++ K4Color_vis.py
@@ -0,0 +1,132 @@
+import numpy as np
+from scipy.special import comb
+from itertools import combinations
+import numpy as np
+import matplotlib.pyplot as plt
+
+def ExpWeight(ColorMat):
+    # compute the conditional expectations
+    E = 0
+    n, n1 = ColorMat.shape
+    assert n == n1
+    # loop over all K4s
+    for K4 in combinations(range(n), 4):
+        indicator = 0  # define the indicator variable
+        numW, numB, numN = 0, 0, 0
+        for edge in combinations(list(K4), 2):
+            i, j = edge
+            if ColorMat[i, j] == 1:
+                numW += 1
+            elif ColorMat[i, j] == -1:
+                numB += 1
+            else:
+                numN += 1
+        assert (numW + numB + numN) == 6
+
+        if (numW != 0) & (numB != 0):
+            indicator = 0
+        elif (numW == 0) & (numB == 0):
+            indicator = (2.0) ** (-5)
+        else:
+            indicator = (2.0) ** (-1 * numN)
+
+        E = E + indicator
+    return E
+
+
+def Count_K4(ColorMat):
+    # count the number of K4
+    cnt = 0
+    n, n1 = ColorMat.shape
+    assert n == n1
+    for K4 in combinations(range(n), 4):
+        numW, numB, numN = 0, 0, 0
+        for edge in combinations(list(K4), 2):
+            i, j = edge
+            if ColorMat[i, j] == 1:
+                numW += 1
+            elif ColorMat[i, j] == -1:
+                numB += 1
+            else:
+                numN += 1
+        assert (numW + numB + numN) == 6
+        if (numW == 6) | (numB == 6):
+            cnt += 1
+    return cnt
+
+def plotGraph():
+    plt.scatter(Position[:, 0], Position[:, 1], marker="o", color='black')
+    plt.xlabel('x_1')
+    plt.ylabel('x_2')
+    n, n1 = ColorMat.shape
+    assert n == n1
+    color = 'none'
+    for edge in combinations(range(n), 2):
+        i, j = edge
+        if ColorMat[i][j] == 1:
+            color = 'blue'
+        elif ColorMat[i][j] == -1:
+            color = 'red'
+        else:
+            color = 'none'
+
+        if color != 'none':
+            plt.plot([Position[i][0], Position[j][0]], [Position[i][1], Position[j][1]], color=color)
+    plt.show()
+
+
+# Initialization
+n = 20  # num of vertices
+ColorMat = np.zeros((n, n))  # the matrix to denote the color strategy. 0:not colored; 1:white; -1:black
+numExp = comb(n, 4) * (2 ** (-5))  # the expectation of the number of K4
+E_prev = numExp  # examine if E_prev == (Ew + Eb) / 2
+
+# Visualizing
+Position = np.random.rand(n, 2)
+Plot = True
+if Plot:
+    plt.scatter(Position[:, 0], Position[:, 1], marker="o", color='black')
+    plt.xlabel('x_1')
+    plt.ylabel('x_2')
+plt.ion()
+
+for num, edge in enumerate(combinations(range(n), 2)):
+    if (num + 1) % 20 == 0:
+        print('Number of colored edges:', num + 1)
+    i, j = edge
+    # try if colored white
+    ColortempW = np.copy(ColorMat)
+    ColortempW[i, j] = 1
+    ColortempW[j, i] = 1
+    Ew = ExpWeight(ColortempW)  # compute the expectation conditioned on that edge is colored white
+    # try if colored black
+    ColortempB = np.copy(ColorMat)
+    ColortempB[i, j] = -1
+    ColortempB[j, i] = -1
+    Eb = ExpWeight(ColortempB)  # compute the expectation conditioned on that edge is colored black
+    # De-randomization
+    ColorMat = ColortempW if (Ew <= Eb) else ColortempB
+    assert E_prev == (Ew + Eb) / 2
+    E_prev = min(Ew, Eb)
+    # Visualizing
+    if Plot:
+        if ColorMat[i][j] == 1:
+            color = 'blue'
+        elif ColorMat[i][j] == -1:
+            color = 'red'
+        else:
+            color = 'none'
+        if color != 'none':
+            plt.plot([Position[i][0], Position[j][0]], [Position[i][1], Position[j][1]], color=color)
+            plt.show()
+            plt.pause(0.05)
+plt.ioff()
+
+print('===========================================')
+print('The number of the vertices:', n)
+print('The number of K4:', int(comb(n, 4)))
+print('The number of K4 of same colored edges after de-randomization:', Count_K4(ColorMat))
+print("The derived expectation number of K4 of same colored edges:", int(numExp))
+print('===========================================')
+if Plot:
+    plotGraph()
